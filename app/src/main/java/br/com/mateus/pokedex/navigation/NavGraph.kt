307aadffbc9ac90ced482668086be61a6c486264
mateus <mateus.silvafreitas27@gmail.com>
2025-04-25 22:28:00 -0300

    remove import

--- app/src/main/java/br/com/mateus/pokedex/navigation/NavGraph.kt
+++ app/src/main/java/br/com/mateus/pokedex/navigation/NavGraph.kt
@@ -1,7 +1,6 @@
 package br.com.mateus.pokedex.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
